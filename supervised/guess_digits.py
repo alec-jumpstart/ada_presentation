9201a4e4741c3fbc901abfef6ed082e053f011b4
Alec Johnson <alec@jumpstart.me>
2021-05-24 17:12:06 -0700

    swap original and new

--- supervised/guess_digits.py
+++ supervised/guess_digits.py
@@ -6,40 +6,38 @@ from sklearn.model_selection import train_test_split
 digits = datasets.load_digits()
 
 _, axes = plt.subplots(nrows=1, ncols=4, figsize=(10, 3))
-for ax, image, label in zip(axes, digits.images, digits.target):
-    ax.set_axis_off()
-    ax.imshow(image, cmap=plt.cm.gray_r, interpolation='nearest')
-    ax.set_title('Training: %i' % label)
 
-# flatten the images
+for ax, image, label in zip(axes, digits.images, digits.target):
+    ax.imshow(image, cmap=plt.cm.gray_r, interpolation='nearest')
+    ax.set_title(f'Training: {label}')
+
+# flatten the images (turns them from an 8x8 grid to a 64x1 list)
 n_samples = len(digits.images)
 data = digits.images.reshape((n_samples, -1))
-
-# Create a classifier: a support vector classifier
-clf = svm.SVC(gamma=0.001)
+# Create a model: a support vector classifier
+classifier = svm.SVC(gamma=0.001)
 
 # Split data into 50% train and 50% test subsets
-X_train, X_test, y_train, y_test = train_test_split(
+x_train, x_test, y_train, y_test = train_test_split(
     data, digits.target, test_size=0.5, shuffle=False)
 
 # Learn the digits on the train subset
-clf.fit(X_train, y_train)
+classifier.fit(x_train, y_train)
 
 # Predict the value of the digit on the test subset
-predicted = clf.predict(X_test)
+predicted = classifier.predict(x_test)
 
 _, axes = plt.subplots(nrows=1, ncols=4, figsize=(10, 3))
-for ax, image, prediction in zip(axes, X_test, predicted):
-    ax.set_axis_off()
-    image = image.reshape(8, 8)
+for ax, image, prediction in zip(axes, x_test, predicted):
+    # image = image.reshape(8, 8)
     ax.imshow(image, cmap=plt.cm.gray_r, interpolation='nearest')
     ax.set_title(f'Prediction: {prediction}')
 
-print(f"Classification report for classifier {clf}:\n"
-      f"{metrics.classification_report(y_test, predicted)}\n")
+print(f"Classification report for classifier {classifier}:")
+print(f"{metrics.classification_report(y_test, predicted)}")
+print()
 
-disp = metrics.plot_confusion_matrix(clf, X_test, y_test)
+disp = metrics.plot_confusion_matrix(classifier, x_test, y_test)
 disp.figure_.suptitle("Confusion Matrix")
-print(f"Confusion matrix:\n{disp.confusion_matrix}")
 
 plt.show()
