e47f5cf2c16f6e42a3bd8a7c8833c2e1317318a4
Alec Johnson <alec@jumpstart.me>
2021-05-26 11:33:53 -0700

    add hotdog test

---
+++ hotdog/test.py
@@ -0,0 +1,18 @@
+import os
+import time
+
+from fastai.imports import plt
+from matplotlib.pyplot import hot
+
+hotdogs = os.listdir('./data/testing/hotdog')[:10]
+for i in range(5):
+    img = plt.imread(f'./data/testing/hotdog/{hotdogs[i]}')
+    plt.imshow(img)
+    plt.show()
+
+
+not_hotdogs = os.listdir('./data/testing/not_hotdog')[:10]
+for i in range(5):
+    img = plt.imread(f'./data/testing/not_hotdog/{not_hotdogs[i]}')
+    plt.imshow(img)
+    plt.show()
